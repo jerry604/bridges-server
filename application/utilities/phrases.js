f95ad0329b5252f23d751c12df45c9701a9560d7
nickboudreau <nr.boudreau@gmail.com>
2016-11-06 13:20:27 -0800

    sphinx

--- application/utilities/phrases.js
+++ application/utilities/phrases.js
@@ -1,8 +1,6 @@
 'use strict';
 
 exports.phrases = [
-    'You have a small dick.',
-    '8========D~~~~~~~~',
     '#TRUMP2016',
     '#HILARY2016',
     'I\'m a vegan.',
@@ -11,8 +9,6 @@ exports.phrases = [
     'Has anybody ever told you that you are incredibly average?',
     'Your teeth throw up gang signs.',
     'You\'re like the Forrest Gump that never did anything',
-    'Climb a wall of dicks.',
-    'Your momma is so ugly, even her dildo needs viagra.',
     'Drama just follows me.',
-    
+    'You\' suck.'
 ];
